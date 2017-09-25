6b3d31448aaefe64edeb2a0cf83eb68d483d234b
chensj@szljfkj.com <csj137982465>
2017-09-25 17:32:17 +0800

    显示1000条

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -339,7 +339,7 @@ class ArticleController extends Controller
      */
     public function getCommet(Request $request, $id)
     {
-        $page_num = (int) $request->input('page_num', env('PAGE_NUM', 100));
+        $page_num = (int) $request->input('page_num', env('PAGE_NUM', 1000));
 
         $db = \DB::table('article_comments')->where('article_id', $id)->where('status', 1)->orderby('add_time','asc');
         $data = $db->paginate($page_num)->toArray();
